b22677b3f5a1f9a1f17d1148dfdf6ff7aedfc3b9
Anna Choi <anna@Annas-MacBook-Air.local>
2019-06-15 12:11:36 +1200

    add metatags

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Layout from '../components/layout';
+
 import Img from 'gatsby-image';
 import Metatags from '../components/Metatags';
-import { graphql } from 'gatsby'
+import {graphql} from 'gatsby';
+
 function BlogPost(props) {
     const post = props.data.markdownRemark;
-    const url = props.data.site.siteMetadata.siteUrl
-    const { title, description } = post.frontmatter;
-    const thumbnail = post.frontmatter.image.childImageSharp.resize.src
+    const url = props.data.site.siteMetadata.siteUrl;
+    const {title, description} = post.frontmatter;
+    const thumbnail = post.frontmatter.image.childImageSharp.resize.src;
+
     return (
         <Layout>
             <Metatags
@@ -19,38 +22,39 @@ function BlogPost(props) {
             />
             <div>
                 <h1>{title}</h1>
-                <Img fluid={post.frontmatter.image.childImageSharp.fluid} />
-                <div dangerouslySetInnerHTML={{ __html: post.html }} />
+                <Img fluid={post.frontmatter.image.childImageSharp.fluid}/>
+                <div dangerouslySetInnerHTML={{ __html: post.html}}>
+                </div>
             </div>
         </Layout>
     )
 }
 
-export default BlogPost
+export default BlogPost;
 
 export const query = graphql`
- query PostQuery($slug: String!) {
-     markdownRemark(fields: { slug: { eq: $slug } }) {
-       html
-       frontmatter {
+    query PostQuery($slug: String!) {
+        markdownRemark(fields: { slug: { eq: $slug } }) {
+        html
+        frontmatter {
         title
         description
         image {
-          childImageSharp {
+            childImageSharp {
             resize(width: 1500, height: 1500) {
-              src
+                src
             }
             fluid(maxWidth: 786) {
-              ...GatsbyImageSharpFluid
+                ...GatsbyImageSharpFluid
             }
-          }
-       }
-       }
-   }
-  site {
-    siteMetadata {
-        siteUrl
-      }
-   }
-}
+            }
+        }
+        }
+    }
+    site {
+        siteMetadata {
+            siteUrl
+            }
+        }
+    }
 `
